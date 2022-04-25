3ef1278968862323dbc51874aba4029bfe8cd409
kat <firstovakatya42@gmail.com>
2022-04-25 18:34:57 +0300

    add lombok

--- src/main/java/rest/model/User.java
+++ src/main/java/rest/model/User.java
@@ -1,18 +1,16 @@
 package rest.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "users")
 
 public class User {
-
-    public User(String name, String phone, String email, Integer id) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.id = id;
-    }
 
     @Column(name = "name")
     private String name;
@@ -27,54 +25,5 @@ public class User {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    public User() {
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) && Objects.equals(phone, user.phone)
-                && Objects.equals(email, user.email) && Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, phone, email, id);
-    }
 }
+
